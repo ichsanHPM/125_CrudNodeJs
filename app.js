738be97ae3041e10a2f19fa193f3c7371d614cef
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-11-07 19:13:20 +0700

    dbcrud_125

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 const express = require('express');
-const todosRoutes = require('./routes/data')
+const todosRoutes = require('./routes/tododb.js');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 
-app.set('view engine', 'ejs')
-app.use(express.json());
-app.use('/todos', todosRoutes)
+const db = require('./database/db.js');
 
-app.get('/', (req, res) => {
-    res.render('index');
+const expressLayouts = require('express-ejs-layouts')
+
+app.use(expressLayouts)
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
+
+app.use(express.json());
+
+app.use('/todos', todosRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index', {
+        layout: 'layouts/main-layout'
+    });
+})
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout: 'layouts/main-layout',
+    });
 });
 
-app.listen(port,()=> {
-    console.log(`server berjalan di http://localhost:${port}`);
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found')
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
 });
--- routes/data.js
+++ routes/data.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
 const router = express.Router();
 
 let todos = [
     {
-        id: 1, task: 'Belajar Node.js', completed: false
+        id: 1, 
+        task: 'Belajar Node.js', 
+        completed: false
     },
     {
-        id: 2, task: 'Membuat API', completed: false
+        id: 2, 
+        task: 'Membuat API',
+         completed: false
     },
 ];
 
